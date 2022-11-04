0a1038af63486065bbe58d0d7c4c015fb69473bf
gzili <gustas.zilinskas@gmail.com>
2022-11-04 16:31:51 +0200

    initial commit

---
+++ VisualRiders.PointOfSale.Project/Models/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace VisualRiders.PointOfSale.Project.Models;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
