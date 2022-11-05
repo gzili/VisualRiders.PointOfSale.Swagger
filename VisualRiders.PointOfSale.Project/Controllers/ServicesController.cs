0da3953ee8fddd2fc10ffe44045bb6c2957a320e
GabijaG <gabija.gakaite@mif.stud.vu.lt>
2022-11-05 22:22:13 +0200

    Create Service API
    Added the `ServicesController` with necessary endpoints as perceived from (3.1.1) user stories.
    
    Added  `ServicesRepository` serving dummy data and implementing methods needed to make endpoints interactive.

---
+++ VisualRiders.PointOfSale.Project/Controllers/ServicesController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+using VisualRiders.PointOfSale.Project.Models;
+using VisualRiders.PointOfSale.Project.Repositories;
+
+namespace VisualRiders.PointOfSale.Project.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServicesController : ControllerBase
+    {
+        private readonly ServicesRepository _servicesRepository;
+
+        public ServicesController(ServicesRepository servicesRepository)
+        {
+            _servicesRepository = servicesRepository;
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201)]
+        public ActionResult<Service> Create(Service service)
+        {
+            _servicesRepository.Create(service);
+
+            return CreatedAtAction("GetById", new { id = service.Id }, service);
+        }
+
+
+        public List<Service> GetAll()
+        {
+            return _servicesRepository.GetAll();
+        }
+
+        [HttpGet("{status:int}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult<List<Service>> GetAllByStatus(int status)
+        {
+            var services = _servicesRepository.GetAllByStatus(status);
+
+            if(services == null)
+            {
+                return NotFound();
+            }
+
+            return services;
+        }
+
+        [HttpGet("{id:guid}")]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult<Service> GetById(Guid id)
+        {
+            var service = _servicesRepository.GetById(id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return service;
+        }
+
+        [HttpPut]
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        public ActionResult<PurchasableItem> UpdateService(Service service)
+        {
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            _servicesRepository.UpdateItem(service);
+            return Ok();
+        }
+
+        [HttpDelete("{id:guid}")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public ActionResult<PurchasableItem> DeleteById(Guid id)
+        {
+            if (_servicesRepository.GetById(id) == null)
+            {
+                return NotFound();
+            }
+
+            _servicesRepository.DeleteById(id);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ VisualRiders.PointOfSale.Project/Repositories/ServicesRepository.cs
@@ -0,0 +1,70 @@
+﻿using VisualRiders.PointOfSale.Project.Models;
+
+namespace VisualRiders.PointOfSale.Project.Repositories
+{
+    public class ServicesRepository
+    {
+        private static List<Service> _services = new()
+        {
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Service 1",
+                Description = "Description 1",
+                Price = 1.0M,
+                Type = Enums.ServiceType.Type2,
+                Status = Enums.ServiceStatus.Deleted,
+                DiscountId = Guid.NewGuid(),
+                BranchId = Guid.NewGuid()
+            },
+            new()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Service 2",
+                Description = "Description 2",
+                Price = 2.0M,
+                Type = Enums.ServiceType.Type2,
+                Status = Enums.ServiceStatus.Active,
+                DiscountId = Guid.NewGuid(),
+                BranchId = Guid.NewGuid()
+            }
+        };
+
+        public void Create(Service service)
+        {
+            _services.Add(service);
+        }
+
+        public List<Service> GetAll()
+        {
+            return _services;
+        }
+
+        public List<Service> GetAllByStatus(int status)
+        {
+            return _services.FindAll(s => (int)s.Status == status);
+        }
+
+        public Service? GetById(Guid id) => _services.Find(s => s.Id == id);
+
+        public void UpdateItem(Service service)
+        {
+            var index = _services.FindIndex(s => s.Id == service.Id);
+
+            if (index != -1)
+            {
+                _services[index] = service;
+            }
+        }
+
+        public void DeleteById(Guid id)
+        {
+            var item = _services.Find(p => p.Id == id);
+
+            if (item != null)
+            {
+                _services.Remove(item);
+            }
+        }
+    }
+}
