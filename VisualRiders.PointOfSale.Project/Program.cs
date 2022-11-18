575c5dfca597aaf4549e538461447ce5b49aabd2
gzili <gustas.zilinskas@gmail.com>
2022-11-19 00:47:19 +0200

    enable Swagger in prod

--- VisualRiders.PointOfSale.Project/Program.cs
+++ VisualRiders.PointOfSale.Project/Program.cs
@@ -33,11 +33,12 @@ builder.Services.AddScoped<EmployeesRepository>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(options =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
 
 // app.UseHttpsRedirection();
 
